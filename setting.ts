34d0962f88a0bae1aa65de09f1f0f7ab33c9939c
mei-y-0616 <s2421216@s.do-johodai.ac.jp>
2025-04-02 10:26:16 +0000

    setup ogp 3

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://https://mei-y-0616.github.io/tech-blog.github.io/";
+export const TARGET_URL = "https://mei-y-0616.github.io/tech-blog.github.io/";
